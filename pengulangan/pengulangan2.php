8d03691aaa60ba41072a1422790abb60ef8a0ce8
mila-28 <mnovia315@gmail.com>
2025-09-15 13:54:01 +0700

    pengulangan
    sintaks php pengulangan

---
+++ pengulangan/pengulangan2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>pengulangan while</title>
+</head>
+<body>
+	<h1>pengulangan</h1>
+	<h2>For</h2>
+	<?php 
+	for ($i=1; $i < 5; $i++) { 
+		echo"hello worlr<br>";
+	}
+
+	 ?>
+
+	 <h2>For</h2>
+	 <?php
+	 $i = 1;
+	 while ($i<=5) {
+	  	echo"hello worlr<br>";
+	  	$i++;
+	  }while($i<=5); 
+
+	  ?>
+
+	  
+
+</body>
+</html>
