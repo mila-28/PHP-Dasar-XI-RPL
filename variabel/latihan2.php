c4df162ca709d13c718cb1cd725710f9f3dc6c6f
mila-28 <mnovia315@gmail.com>
2025-09-15 13:49:26 +0700

    variabel
    sintaks php variabel

---
+++ variabel/latihan2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>variabel latihan 2</title>
+</head>
+<body>
+	<?php 
+	    $nama="mila";
+	    $kelas="xi rpl";
+	    $alamat="korea";
+
+	    echo"nama : $nama";
+	    echo"<br>";
+	    echo"kelas : $kelas";
+	    echo"<br>";
+	    echo"alamat : $alamat";
+	 ?>
+
+</body>
+</html>
